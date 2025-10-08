f94f6600825dd252afd383c12e11e7b30bd56d87
Gabriella Parra <gabriella.parra05@gmail.com>
2025-10-08 11:26:41 -0300

    main e module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_up_body',
+      entities: [Produtos, Categorias, Usuario],
+      synchronize: true,
+    }),
+    ProdutoModule,
+    CategoriaModule,
+    UsaurioModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
